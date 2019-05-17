ac2db591afc2e936bc56aa5aec40e3adf44c3566
Meghan Nahnybida <meghannahnybida@gmail.com>
2019-05-17 12:30:45 -0500

    working on main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args){
 
-    
+        String [] cardTypes = { };
+
+    String [] playerDeck = new String [25];
+    String [] compDeck = new String [25];
+
+
     }
 }
