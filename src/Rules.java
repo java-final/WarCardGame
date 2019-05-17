ba6ca179bc3f768d26c99102ef0a161fbe84b225
Sym Conger <acesilverryuu@gmail.com>
2019-05-17 13:24:19 -0500

    rules

--- src/Rules.java
+++ src/Rules.java
@@ -1,2 +1,16 @@
 public class Rules {
+
+    public static void main (String args[]){
+
+        System.out.println("Rules of WAR: \n");
+        System.out.println("1. Each player gets dealt 25 cards face down. (Jokers are not included)\n");
+        System.out.println("2. The players pull the top card form their decks " +
+                "and flip them face up at the same time. \n");
+        System.out.println("3. The highest card wins and the winner collects the other players card. " +
+                " IF the cards  \n");
+        System.out.println("4. Set the cards won off to the side. \n");
+        System.out.println("5. Repeat rules 2-4 until both players are out of cards. \n");
+        System.out.println("6. Player with the most cards in the end wins. \n");
+        System.out.println("\n");
+    }
 }
