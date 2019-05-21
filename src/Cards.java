719f02150e5210e7f6e33f59e7f0fa6ff982d70f
Meghan Nahnybida <meghannahnybida@gmail.com>
2019-05-21 12:48:22 -0500

    updating main

--- src/Cards.java
+++ src/Cards.java
@@ -1,14 +1,14 @@
 public class Cards {
 
 
-    public Cards(){
+    public Cards(String suit, String rank){
          String suit;
 //         int rank = 0;
          int value;
          int [] rank = {2,3,4,5,6,7,8,9,10,11,12,13,14};
         int random = (int) (Math.random() * 14 + 1);
 
-         for(int i = 0; i<rank.length; i++){
+         for(int i = 0; i <rank.length; i++){
              if(rank[i] == random) {
                  value = rank[i];
              }
